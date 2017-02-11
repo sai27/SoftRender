af892c6e152f193ae08e45a4c0d5506a1c4225c5
sai27 <yubo.net@163.com>
2017-02-12 04:22:00 +0800

    建立工程

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SoftRender
-cpu simulate the 3d graphic render pipeline for leraning
+cpu simulate the 3d graphic render pipeline for learning
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// *********************************************************************
+// Name: main.cpp
+// Date: 2017/02/12 04:12
+// Author: sai27
+// Description:
+// *********************************************************************
+
+#include<windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	return 0;
+}
